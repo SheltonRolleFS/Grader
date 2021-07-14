9d34f2f6d38d72c9041e245a815ca69996baa912
Shelton Rolle <sheltonrolle@Sheltons-MacBook-Pro-2.local>
2021-07-14 14:23:09 -0400

    Created ruby version of the Grader

---
+++ grader.rb
@@ -0,0 +1,33 @@
+class Grader
+	def initialize()
+		puts "What is your name?"
+		@name = gets
+		puts "What is the name of your assignment?"
+		@assignmentName = gets
+		puts "What is the grade you got in " + @assignmentName + "?"
+		@grade = gets.to_f
+	end
+	
+	def getLetterGrade
+		letter_grade = ''
+		if @grade < 60
+			letter_grade = 'F'
+		elsif @grade < 70
+			letter_grade = 'D'
+		elsif @grade < 80
+			letter_grade = 'C'
+		elsif @grade < 90
+			letter_grade = 'B'
+		else
+			letter_grade = 'A'
+		end
+		
+		return letter_grade
+	end
+
+end
+
+myGrade = Grader.new
+print(myGrade.getLetterGrade())
+puts "------------------------"
+			
